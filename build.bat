8509cc6016d46080fcba4eeea52dde2cf283caea
achalpandeyy <pandeyachal.ap@gmail.com>
2024-01-21 19:19:30 +0000

    Add a build.bat, change some bindings and user name.

--- build.bat
+++ build.bat
@@ -1,4 +1,21 @@
 @echo off
 call ..\bin\buildsuper_x64-win.bat .\4coder_fleury.cpp release
-copy .\custom_4coder.dll ..\..\custom_4coder.dll
-copy .\custom_4coder.pdb ..\..\custom_4coder.pdb
+
+pushd ..\..\..\build
+
+if not exist 4coder_fleury mkdir 4coder_fleury
+
+copy ..\code\custom\4coder_fleury\custom_4coder.dll 4coder_fleury\custom_4coder.dll
+copy ..\code\custom\4coder_fleury\custom_4coder.pdb 4coder_fleury\custom_4coder.pdb
+:: TODO(achal): If bindings.4coder and config.4coder already exist don't copy them because we do not want to override user's settings with default settings.
+
+if not exist ..\code\custom\4coder_fleury\bindings.4coder (
+	echo Overwriting bindings..
+	copy ..\code\custom\4coder_fleury\bindings.4coder 4coder_fleury\bindings.4coder
+)
+if not exist ..\code\custom\4coder_fleury\config.4coder (
+	echo Overwriting config..
+	copy ..\code\custom\4coder_fleury\config.4coder 4coder_fleury\config.4coder
+)
+
+popd
